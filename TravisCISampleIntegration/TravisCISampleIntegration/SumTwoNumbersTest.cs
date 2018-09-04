0b170447f62a8e4a75c44c22cc8690753d24df2b
zombiecmp <zombie.cmp@gmail.com>
2018-09-03 21:46:13 -0400

    Add Test Prj

--- TravisCISampleIntegration/TravisCISampleIntegration/SumTwoNumbersTest.cs
+++ TravisCISampleIntegration/TravisCISampleIntegration/SumTwoNumbersTest.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
 
 namespace TravisCISampleIntegration
 {
     public class SumTwoNumbersTest
     {
-        [Fact]
+        //[Fact]
         public void PassingTest()
         {
             Assert.Equal(4, SumTwoNumbers.Add(2, 2));
         }
 
-        [Fact]
+        //[Fact]
         public void FailingTest()
         {
             Assert.Equal(5, SumTwoNumbers.Add(2, 2));
---
+++ TravisCISampleIntegration/TravisSampleTest/AddTwoNumbersTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace TravisSampleTest
+{
+    public class AddTwoNumbersTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}

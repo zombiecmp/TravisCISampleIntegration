0fc956475665682bffd6caecd570f4d3349b8b38
zombiecmp <zombie.cmp@gmail.com>
2018-09-04 13:37:09 -0400

    Test Proj net45

---
+++ TravisCISampleIntegration/SumUtils.Test/AddTwoNumbersTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace SumUtils.Test
+{
+    public class AddTwoNumbersTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, SumTwoNumbers.Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, SumTwoNumbers.Add(2, 2));
+        }
+    }
+}

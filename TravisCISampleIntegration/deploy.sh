2d66564193faacd9d65880f8be5d31d79d5903c9
zombiecmp <zombie.cmp@gmail.com>
2018-09-05 12:52:17 -0400

    Path

--- TravisCISampleIntegration/deploy.sh
+++ TravisCISampleIntegration/deploy.sh
@@ -1,6 +1,6 @@
 ApiKey=$1
 Source=$2
 
-nuget pack SumUtils/SumUtils.nuspec -Verbosity detailed
+nuget pack ./TravisCISampleIntegration/SumUtils/SumUtils.nuspec -Verbosity detailed
 
-nuget push SumUtils/SumUtils.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+nuget push ./TravisCISampleIntegration/SumUtils/SumUtils.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source

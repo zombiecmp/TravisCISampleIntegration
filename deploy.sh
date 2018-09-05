7ffd97dcc1298925ccc3df82e80c2d3705114290
zombiecmp <zombie.cmp@gmail.com>
2018-09-05 12:04:06 -0400

    Deploy config

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+ApiKey=$1
+Source=$2
+
+nuget pack ./TravisCISampleIntegration/SumUtils/SumUtils.nuspec -Verbosity detailed
+
+nuget push ./TravisCISampleIntegration/SumUtils/SumUtils.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source

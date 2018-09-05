e4b8a63c738adc09e4b77c88ed3be41af34dd75f
zombiecmp <zombie.cmp@gmail.com>
2018-09-05 13:51:51 -0400

    Update nuget package name

--- TravisCISampleIntegration/deploy.sh
+++ TravisCISampleIntegration/deploy.sh
@@ -3,4 +3,4 @@ Source=$2
 
 nuget pack ./TravisCISampleIntegration/SumUtils/SumUtils.nuspec -Verbosity detailed
 
-nuget push ./TravisCISampleIntegration/SumUtils/SumUtils.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+nuget push ./TravisCISampleIntegration/SumUtils/SumUtils.1.0.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
